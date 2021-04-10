2d3ac69c9230d64fcb4f36f244af746a73f2fd29
ShardulJoshi05 <shardulj.nmims@gmail.com>
2021-04-10 23:40:31 +0530

    Completed checkpoint 11

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+function view() {
+    var toggle = document.getElementById("vm");
+    if (toggle.textContent === "View More") {
+      document.getElementById("vmore").style.display = "flex";
+      toggle.innerText = "View Less";
+    } else {
+      document.getElementById("vmore").style.display = "none";
+      toggle.innerText = "View More";
+    }
+  }
